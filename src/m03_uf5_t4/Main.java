039e651bfad2e6413f7a1ceadaef6c056a255ffd
Qu4k3 <paul.guillamon@gmail.com>
2017-11-02 15:18:16 +0100

    clean java app

---
+++ src/m03_uf5_t4/Main.java
@@ -0,0 +1,13 @@
+
+package m03_uf5_t4;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
