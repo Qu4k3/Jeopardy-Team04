0739874cb54374160ded5aa8a0526c2701612ffe
DiegoSC94 <diego.santamarta@gracia.lasalle.cat>
2017-11-09 16:24:59 +0100

    Estructura base acabada

---
+++ src/utils/EnumValueBox.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Diego
+ */
+public enum EnumValueBox {
+    //200, 400, 600, 800, 1000
+}
