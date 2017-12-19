7e3bc4e26f33b238e5460c006da636f9c40fb7d7
Qu4k3 <paul.guillamon@gmail.com>
2017-12-19 23:19:08 +0100

    solved

--- src/controller/WinnerController.java
+++ src/controller/WinnerController.java
@@ -13,6 +13,18 @@ public class WinnerController implements ActionListener {
     public WinnerController(PlayerModel model, WinnerView view) {
         this.model = model;
         this.view = view;
+        
+        //Set view data
+        this.view.player1.setText("Enhorabuena, " + "player_name!");
+        
+        this.view.player1.setText("player1_name");
+        this.view.player2.setText("player2_name");
+        
+        this.view.wallet1.setText("$200");
+        this.view.wallet2.setText("$500");
+        
+        this.view.mistakes1.setText("7");        
+        this.view.mistakes2.setText("5");
 
         //Add listeners to the view
         this.view.retryButton.addActionListener(this);
@@ -22,7 +34,7 @@ public class WinnerController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == this.view.retryButton) {
-            //this.
+            //new PlayerDataController(, view);
         } else if (ae.getSource() == this.view.exitButton) {
             System.exit(0);
         }
